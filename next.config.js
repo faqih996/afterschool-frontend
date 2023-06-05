614a6ccd12c0754f8e4bc4ddc1205c0e7bac919b
Faqih996 <faqih.syakir11@gmail.com>
2023-06-05 23:32:10 +0700

    first init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+const withPlugins = require("next-compose-plugins");
+const withCss = require("@zeit/next-css");
+const withReactSvg = require("next-react-svg");
+const withImages = require("next-images");
+
+const path = require("path")
+
+module.exports = withPlugins([
+    withCss({}),
+    withImages({}),
+    withReactSvg({
+        include: path.resolve(__dirname, "./public/src/svg"),
+        webpack(config, options) {
+            return config;
+        }
+    })
+]);
---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <div className="container mx-auto mt-4">
+            <Head>
+                <title>
+                    After School
+                </title>
+                <link rel="icon" href="/favicon.icon" />
+            </Head>
+
+            <main>
+                <h1>Halaman utama</h1>
+            </main>
+        </div>
+    )
+}
