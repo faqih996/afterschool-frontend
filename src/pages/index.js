57a0f4a525e4a9d8b1c185dd83e37ae015075063
primaandalansumberdaya <admin@ptpas.id>
2023-08-13 16:31:10 +0700

    install babelrc

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,25 +22,25 @@ function Home(props) {
                     <div className="max-w-full sunshine"></div>
                         <Circle className="absolute bottom-0 left-0"></Circle>
                     <div className="container px-4 mx-auto">
-                        <Header></Header>
-                        <Hero></Hero>
+                        {/* <Header></Header> */}
+                        {/* <Hero></Hero> */}
                     </div>
                 </section>
 
                 <section className="container px-4 mx-auto md:pt-24">
-                    <Clients></Clients>
+                    {/* <Clients></Clients> */}
                 </section>
 
                 <section className="container px-4 mx-auto md:pt-24">
-                    <ListCourses data={data}></ListCourses>
+                    {/* <ListCourses data={data}></ListCourses> */}
                 </section>
 
                 <section className="container px-4 mx-auto md:pt-24">
-                    <ListCategories></ListCategories>
+                    {/* <ListCategories></ListCategories> */}
                 </section>
 
                 <section className="py-12 mt-24 bg-indigo-1000">
-                    <Footer></Footer>
+                    {/* <Footer></Footer> */}
                 </section>
             </main>
         </div>
